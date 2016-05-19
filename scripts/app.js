09f26670ebaf514cb36e21f704d3b313338a16ec
Lena <neverbalnost@gmail.com>
2016-05-19 23:34:48 +0300

    fix issue

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ solar.controller('diagram', function ($scope, $http) {
     $http({ method: 'GET', url: 'data.json'})
             .then(function successCallback(response) {
                 $scope.planets = response.data;
-                $scope.names = $scope.planets[0]["v"]
+                $scope.names = $scope.planets.col[0].v;
               }, function errorCallback(response) {
                   console.log(response);
               });
