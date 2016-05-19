889b44a79fed3a551c036cdcd555ad4fe5003412
Lena <neverbalnost@gmail.com>
2016-05-20 00:48:38 +0300

    try to fix filtering

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,31 @@ var solar = angular.module('solar', []);
 
 solar.controller('diagram', function ($scope, $http) {
     $http({ method: 'GET', url: 'data.json'})
-            .then(function successCallback(response) {
-                $scope.planets = response.data;
-                $scope.names = $scope.planets.col[0].v;
-              }, function errorCallback(response) {
-                  console.log(response);
-              });
+        .then(function successCallback(response) {
+            $scope.planets = response.data;
+            $scope.names = $scope.planets.col[0].v;
+        }, function errorCallback(response) {
+            console.log(response);
+        });
+    
+    $scope.planetIncludes = [];
+    
+    $scope.includePlanet = function(planet) {
+        var i = $.inArray(planet, $scope.planetIncludes);
+        if (i > -1) {
+            $scope.planetIncludes.splice(i, 1);
+        } else {
+            $scope.planetIncludes.push(planet);
+        }
+    }
+    
+    $scope.planetFilter = function(planet) {
+        if ($scope.colourIncludes.length > 0) {
+            if ($.inArray($scope.names, $scope.planetIncludes) < 0)
+                return;
+        }
+        
+        return planet;
+    }
     
 })
