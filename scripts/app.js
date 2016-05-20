58bbb1827015ca386a34f2b5ea39c0dc434b752b
Lena <neverbalnost@gmail.com>
2016-05-20 03:34:15 +0300

    fix issue

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,12 @@
 var solar = angular.module('solar', ['ngRoute', 'solarController']);
 
+solar.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/mass', {
+        templateUrl: 'partials/mass.html'
+      }).
+      otherwise({
+        redirectTo: '/mass'
+      });
+  }]);
