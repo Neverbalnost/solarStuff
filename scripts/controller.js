ccaad5436a4bc392226c652d8b2efad5117811f9
Elena <Elena_Kogodeeva@epam.com>
2016-05-20 15:34:45 +0300

    try to fix column heights

--- scripts/controller.js
+++ scripts/controller.js
@@ -4,15 +4,32 @@ var solarController = angular.module('solarController', []);
 solarController.controller('diagram', function ($scope, $http) {
     $http({ method: 'GET', url: 'data.json'})
         .then(function successCallback(response) {
-            $scope.planets = []
-            for (var i = 0; i< response.data.col[0].v.length; i++) {
+            $scope.planets = [];
+			
+			for (var i = 1; i < response.data.col.length; i++) {
+				
+				var values = [],
+					max,
+					percents = [];
+					
+				response.data.col[i].v.forEach(function(item) {values.push(parseInt(item.replace(/,/g, ""), 10))});
+				
+				max = Math.max.apply(null, values);
+					
+				response.data.col[i].v.forEach(function(val) {
+					percents.push(val*100/max);
+				})
+				response.data.col[i].p = percents;
+			}
+			
+            for (var i = 0; i < response.data.col[0].v.length; i++) {
                 var planet = {
-                    name: {value: response.data.col[0].v[i], label: response.data.col[0].col.label},
-                    mass: {value: response.data.col[1].v[i], label: response.data.col[1].col.label},
-                    diameter: {value: parseInt(response.data.col[2].v[i].replace(/,/g, ""), 10)/1000, label: response.data.col[2].col.label},
-                    density: {value: parseInt(response.data.col[3].v[i].replace(/,/g, ""), 10)/100, label: response.data.col[3].col.label},
-                    gravity: {value: response.data.col[4].v[i], label: response.data.col[4].col.label},
-                    temperature: {value: response.data.col[5].v[i], label: response.data.col[5].col.label},
+                    name: {value: response.data.col[0].v[i], height: response.data.col[0].p[i],  label: response.data.col[0].col.label},
+                    mass: {value: response.data.col[1].v[i], height: response.data.col[1].p[i],  abel: response.data.col[1].col.label},
+                    diameter: {value: response.data.col[2].v[i], height: response.data.col[2].p[i],  label: response.data.col[2].col.label},
+                    density: {value: response.data.col[3].v[i], height: response.data.col[3].p[i],  label: response.data.col[3].col.label},
+                    gravity: {value: response.data.col[4].v[i], height: response.data.col[4].p[i],  label: response.data.col[4].col.label},
+                    temperature: {value: response.data.col[5].v[i], height: response.data.col[5].p[i], label: response.data.col[5].col.label},
                 }
                 $scope.planets.push(planet);
             }
