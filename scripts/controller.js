d5254e6b8877c5c659ace78ced385ced4722a042
Elena <Elena_Kogodeeva@epam.com>
2016-05-20 15:40:58 +0300

    fix issue

--- scripts/controller.js
+++ scripts/controller.js
@@ -24,7 +24,7 @@ solarController.controller('diagram', function ($scope, $http) {
 			
             for (var i = 0; i < response.data.col[0].v.length; i++) {
                 var planet = {
-                    name: {value: response.data.col[0].v[i], height: response.data.col[0].p[i],  label: response.data.col[0].col.label},
+                    name: {value: response.data.col[0].v[i], label: response.data.col[0].col.label},
                     mass: {value: response.data.col[1].v[i], height: response.data.col[1].p[i],  abel: response.data.col[1].col.label},
                     diameter: {value: response.data.col[2].v[i], height: response.data.col[2].p[i],  label: response.data.col[2].col.label},
                     density: {value: response.data.col[3].v[i], height: response.data.col[3].p[i],  label: response.data.col[3].col.label},
